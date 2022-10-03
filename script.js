89afc6042f38cc8644d02ea44343d97e7afb5ed7
MDMS29 <103197915+MDMS29@users.noreply.github.com>
2022-10-03 11:06:55 -0500

    Update Script

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const buttonShow = document.querySelector('.list-objects');
+const main = document.querySelector('main')
 
 buttonShow.addEventListener('click', () =>{
     const sideBar = document.querySelector('.slider-obj');
+    const items = document.querySelector('ul');
+    const titulo = document.querySelector('h2');
     sideBar.classList.toggle('active');
+    items.classList.toggle('active');
+    titulo.classList.toggle('active');
 })
